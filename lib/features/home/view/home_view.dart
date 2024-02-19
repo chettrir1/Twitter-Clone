f77f7219897265a6fc3a434e467ae6dd8716589c
Raju Chettri <dada@Rajus-MacBook-Pro.local>
2024-02-19 15:09:29 -0500

    bottom navigation bar added

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,13 +1,56 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/theme/theme.dart';
 
-class HomeView extends ConsumerWidget {
+class HomeView extends StatefulWidget {
   static route() => MaterialPageRoute(builder: (context) => const HomeView());
 
   const HomeView({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold();
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final appBar = UiConstants.appBar();
+  int _page = 0;
+
+  void onPageChange(int index) {
+    setState(() {
+      _page = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar,
+      bottomNavigationBar: CupertinoTabBar(
+        currentIndex: _page,
+        onTap: onPageChange,
+        items: [
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                  (_page == 0)
+                      ? AssetsConstants.homeFilledIcon
+                      : AssetsConstants.homeOutlinedIcon,
+                  colorFilter: const ColorFilter.mode(
+                      Pallete.whiteColor, BlendMode.srcIn))),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(AssetsConstants.searchIcon,
+                  colorFilter: const ColorFilter.mode(
+                      Pallete.whiteColor, BlendMode.srcIn))),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                  (_page == 2)
+                      ? AssetsConstants.notifFilledIcon
+                      : AssetsConstants.notifOutlinedIcon,
+                  colorFilter: const ColorFilter.mode(
+                      Pallete.whiteColor, BlendMode.srcIn)))
+        ],
+      ),
+    );
   }
 }
